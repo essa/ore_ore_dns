430b28b35fbe202e41c240278698a365dfe4875d
Taku Nakajima <tnakajima@degica.com>
2016-03-02 18:57:16 +0900

    Add blank components to top page

--- client/app/bundles/HelloWorld/startup/clientRegistration.jsx
+++ client/app/bundles/HelloWorld/startup/clientRegistration.jsx
@@ -1,5 +1,8 @@
 import ReactOnRails from 'react-on-rails';
 import HelloWorldApp from './HelloWorldAppClient';
+import StatusMessage from '../components/StatusMessage';
+import StartButton from '../components/StartButton';
 
 // This is how react_on_rails can see the HelloWorldApp in the browser.
 ReactOnRails.register({ HelloWorldApp });
+ReactOnRails.register({ StatusMessage, StartButton });
